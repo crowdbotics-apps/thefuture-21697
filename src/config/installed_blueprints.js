d7c8cb89794dbd7200708ae27264a30d51b3c6ad
Crowdbotics <team@crowdbotics.com>
2020-10-19 05:33:55 +0000

    add package blueprint-react-native-sign-in-2-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'SignIn2148999', human_name: 'Sign In 2', access_route: 'SignIn2148999'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import SignIn2148999Reducer from '../features/SignIn2148999/redux/reducers'
 
 export const combinedReducers = combineReducers({
   blank: (state, action) => {
@@ -15,5 +16,6 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+SignIn2148999: SignIn2148999Reducer,
 
 });
